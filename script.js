b8a248364da2937ed5710ee26a913e1380f35686
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-06 13:51:25 +0400

    add: container styling and fetch URL from REST

---
+++ script.js
@@ -0,0 +1,21 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      result.innerHTML = `
+         <img src="${data[0].flags.svg}" class="flag-img">
+         <h2>${data[0].name.common}</h2>
+         <div class="wrapper">
+           <div class="data-wrapper">
+              <h4>Capital: </h4>
+              <span>${data[0].capital[0]}</span>
+           </div>
+         </div>
+        `;
+    });
+});
